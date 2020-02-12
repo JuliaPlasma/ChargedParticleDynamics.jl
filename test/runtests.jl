49c4d3861c4065576bd24c79df38fb41f3a866a4
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-02-12 13:57:15 +0100

    Add solver settings to tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@
 using GeometricIntegrators
 using Test
 
+# solver settings
+# set_config(:nls_atol_break, 1E-3)
+# set_config(:nls_rtol_break, 1E-3)
+set_config(:nls_stol_break, 1E3)
+
+
 include("charged_particle_3d_tests.jl")
 include("guiding_center_4d_tests.jl")
