3538ecbafb10dcf8f84816ea17daf5abe1424646
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-02-14 21:21:35 +0100

    Clean up guiding center module.

--- src/GuidingCenter4d.jl
+++ src/GuidingCenter4d.jl
@@ -1,14 +1,7 @@
 module GuidingCenter4d
 
-    using GeometricIntegrators
-    using ElectromagneticFields
-    using LinearAlgebra
 
 
-    export GuidingCenter4dSolovevIterXpoint,
-           GuidingCenter4dSolovevQuadratic,
-           GuidingCenter4dSymmetricQuadratic,
-           GuidingCenter4dThetaPinch
 
     include("guiding_center_4d/solovev_iter_xpoint.jl")
     include("guiding_center_4d/solovev_quadratic.jl")
@@ -16,19 +9,11 @@ module GuidingCenter4d
     include("guiding_center_4d/theta_pinch.jl")
 
 
-    export TokamakMediumCartesian,
-           TokamakMediumCylindrical,
-           TokamakSmallCylindrical
 
     include("guiding_center_4d/tokamak_medium_cartesian.jl")
     include("guiding_center_4d/tokamak_medium_cylindrical.jl")
     include("guiding_center_4d/tokamak_small_cylindrical.jl")
 
 
-    export convert_coordinates_RZphi_to_xyz,
-           compute_energy, compute_toroidal_momentum, compute_one_form,
-           compute_momentum_error, compute_error_drift
-
-    include("guiding_center_4d/guiding_center_4d_diagnostics.jl")
 
 end
