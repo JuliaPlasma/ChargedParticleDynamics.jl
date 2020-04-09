a1d38bd6a76390fbf101266baf3557ff28f17c62
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-04-09 22:04:16 +0200

    Fix scaling issue.

--- src/gyro_kinetics_4d/coordinate_transformations.jl
+++ src/gyro_kinetics_4d/coordinate_transformations.jl
@@ -7,11 +7,11 @@ export transform_q_to_q̃!, transform_q_to_q̃,
 
 
 function transform_q_to_q̃!(q̃, t, q, params)
-    q̃ .= q .* params[:scaling_factor]
+    q̃ .= q .* params[:ω₀]
 end
 
 function transform_q̃_to_q!(q, t, q̃, params)
-    q .= q̃ ./ params[:scaling_factor]
+    q .= q̃ ./ params[:ω₀]
 end
 
 transform_q_to_q̃(t, q, params) = transform_q_to_q̃!(zero(q), t, q, params)
