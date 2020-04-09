e024b19d21954e32e13b046fec4ab1e7e36cad0b
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-04-09 18:55:35 +0200

    Generalise and fix gyrokinetics equations.

--- src/gyro_kinetics_4d/gc_common.jl
+++ src/gyro_kinetics_4d/gc_common.jl
@@ -33,12 +33,19 @@ end
 d²Hdx₁dx₁(t, q, μ) = μ * d²Bdx₁dx₁(t,q)
 d²Hdx₁dx₂(t, q, μ) = μ * d²Bdx₁dx₂(t,q)
 d²Hdx₁dx₃(t, q, μ) = μ * d²Bdx₁dx₃(t,q)
+d²Hdx₁dx₄(t, q, μ) = μ * d²Bdx₁dx₃(t,q)
 d²Hdx₂dx₁(t, q, μ) = μ * d²Bdx₂dx₁(t,q)
 d²Hdx₂dx₂(t, q, μ) = μ * d²Bdx₂dx₂(t,q)
 d²Hdx₂dx₃(t, q, μ) = μ * d²Bdx₂dx₃(t,q)
+d²Hdx₂dx₄(t, q, μ) = μ * d²Bdx₂dx₃(t,q)
 d²Hdx₃dx₁(t, q, μ) = μ * d²Bdx₃dx₁(t,q)
 d²Hdx₃dx₂(t, q, μ) = μ * d²Bdx₃dx₂(t,q)
 d²Hdx₃dx₃(t, q, μ) = μ * d²Bdx₃dx₃(t,q)
+d²Hdx₃dx₄(t, q, μ) = μ * d²Bdx₃dx₃(t,q)
+d²Hdx₄dx₁(t, q, μ) = zero(eltype(q))
+d²Hdx₄dx₂(t, q, μ) = zero(eltype(q))
+d²Hdx₄dx₃(t, q, μ) = zero(eltype(q))
+d²Hdx₄dx₄(t, q, μ) = one(eltype(q))
 
 
 ϑ₁(t,q) = A₁(t,q) + u(t,q) * b₁(t,q)
@@ -157,6 +164,7 @@ d²ϑ₄dx₄dx₃(t,q) = zero(eltype(q))
 d²ϑ₄dx₄dx₄(t,q) = zero(eltype(q))
 
 
+
 ω₁(t,q) = dϑ₃dx₂(t,q) - dϑ₂dx₃(t,q)
 ω₂(t,q) = dϑ₁dx₃(t,q) - dϑ₃dx₁(t,q)
 ω₃(t,q) = dϑ₂dx₁(t,q) - dϑ₁dx₂(t,q)
@@ -211,21 +219,22 @@ dω₃dx₃(t,q) = d²ϑ₂dx₁dx₃(t,q) - d²ϑ₁dx₂dx₃(t,q)
 dω₃dx₄(t,q) = d²ϑ₂dx₁dx₄(t,q) - d²ϑ₁dx₂dx₄(t,q)
 
 
-β₁(t, q) = u(t,q) * ϑ₁(t,q)
-β₂(t, q) = u(t,q) * ϑ₂(t,q)
-β₃(t, q) = u(t,q) * ϑ₃(t,q)
+β₁(t,q,μ) = dHdx₄(t,q,μ) * ϑ₁(t,q)
+β₂(t,q,μ) = dHdx₄(t,q,μ) * ϑ₂(t,q)
+β₃(t,q,μ) = dHdx₄(t,q,μ) * ϑ₃(t,q)
 
 function β(t, q, β, params)
-    β[1] = β₁(t,q)
-    β[2] = β₂(t,q)
-    β[3] = β₃(t,q)
+    @unpack μ = params
+    β[1] = β₁(t,q,μ)
+    β[2] = β₂(t,q,μ)
+    β[3] = β₃(t,q,μ)
     nothing
 end
 
 
-γ₁(t, q, μ) = ϑ₂(t,q) * dHdx₃(t,q,μ) - dHdx₂(t,q,μ) * ϑ₃(t,q)
-γ₂(t, q, μ) = ϑ₃(t,q) * dHdx₁(t,q,μ) - dHdx₃(t,q,μ) * ϑ₁(t,q)
-γ₃(t, q, μ) = ϑ₁(t,q) * dHdx₂(t,q,μ) - dHdx₁(t,q,μ) * ϑ₂(t,q)
+γ₁(t,q,μ) = ϑ₂(t,q) * dHdx₃(t,q,μ) - dHdx₂(t,q,μ) * ϑ₃(t,q)
+γ₂(t,q,μ) = ϑ₃(t,q) * dHdx₁(t,q,μ) - dHdx₃(t,q,μ) * ϑ₁(t,q)
+γ₃(t,q,μ) = ϑ₁(t,q) * dHdx₂(t,q,μ) - dHdx₁(t,q,μ) * ϑ₂(t,q)
 
 function γ(t, q, γ, params)
     @unpack μ = params
@@ -236,51 +245,54 @@ function γ(t, q, γ, params)
 end
 
 
-dβ₁dx₁(t, q) = u(t,q) * dϑ₁dx₁(t,q)
-dβ₁dx₂(t, q) = u(t,q) * dϑ₁dx₂(t,q)
-dβ₁dx₃(t, q) = u(t,q) * dϑ₁dx₃(t,q)
-dβ₁dx₄(t, q) = ϑ₁(t,q)
-dβ₂dx₁(t, q) = u(t,q) * dϑ₂dx₁(t,q)
-dβ₂dx₂(t, q) = u(t,q) * dϑ₂dx₂(t,q)
-dβ₂dx₃(t, q) = u(t,q) * dϑ₂dx₃(t,q)
-dβ₂dx₄(t, q) = ϑ₂(t,q)
-dβ₃dx₁(t, q) = u(t,q) * dϑ₃dx₁(t,q)
-dβ₃dx₂(t, q) = u(t,q) * dϑ₃dx₂(t,q)
-dβ₃dx₃(t, q) = u(t,q) * dϑ₃dx₃(t,q)
-dβ₃dx₄(t, q) = ϑ₃(t,q)
+dβ₁dx₂(t,q,μ) = d²Hdx₄dx₂(t,q,μ) * ϑ₁(t,q) + dHdx₄(t,q,μ) * dϑ₁dx₂(t,q)
+dβ₁dx₁(t,q,μ) = d²Hdx₄dx₁(t,q,μ) * ϑ₁(t,q) + dHdx₄(t,q,μ) * dϑ₁dx₁(t,q)
+dβ₁dx₃(t,q,μ) = d²Hdx₄dx₃(t,q,μ) * ϑ₁(t,q) + dHdx₄(t,q,μ) * dϑ₁dx₃(t,q)
+dβ₁dx₄(t,q,μ) = d²Hdx₄dx₄(t,q,μ) * ϑ₁(t,q) + dHdx₄(t,q,μ) * dϑ₁dx₄(t,q)
+dβ₂dx₁(t,q,μ) = d²Hdx₄dx₁(t,q,μ) * ϑ₂(t,q) + dHdx₄(t,q,μ) * dϑ₂dx₁(t,q)
+dβ₂dx₂(t,q,μ) = d²Hdx₄dx₂(t,q,μ) * ϑ₂(t,q) + dHdx₄(t,q,μ) * dϑ₂dx₂(t,q)
+dβ₂dx₃(t,q,μ) = d²Hdx₄dx₃(t,q,μ) * ϑ₂(t,q) + dHdx₄(t,q,μ) * dϑ₂dx₃(t,q)
+dβ₂dx₄(t,q,μ) = d²Hdx₄dx₄(t,q,μ) * ϑ₂(t,q) + dHdx₄(t,q,μ) * dϑ₂dx₄(t,q)
+dβ₃dx₁(t,q,μ) = d²Hdx₄dx₁(t,q,μ) * ϑ₃(t,q) + dHdx₄(t,q,μ) * dϑ₃dx₁(t,q)
+dβ₃dx₂(t,q,μ) = d²Hdx₄dx₂(t,q,μ) * ϑ₃(t,q) + dHdx₄(t,q,μ) * dϑ₃dx₂(t,q)
+dβ₃dx₃(t,q,μ) = d²Hdx₄dx₃(t,q,μ) * ϑ₃(t,q) + dHdx₄(t,q,μ) * dϑ₃dx₃(t,q)
+dβ₃dx₄(t,q,μ) = d²Hdx₄dx₄(t,q,μ) * ϑ₃(t,q) + dHdx₄(t,q,μ) * dϑ₃dx₄(t,q)
 
-dγ₁dx₁(t, q, μ) = dϑ₂dx₁(t,q) * dHdx₃(t,q,μ) + ϑ₂(t,q) * d²Hdx₃dx₁(t,q,μ) - d²Hdx₂dx₁(t,q,μ) * ϑ₃(t,q) - dHdx₂(t,q,μ) * dϑ₃dx₁(t,q)
-dγ₁dx₂(t, q, μ) = dϑ₂dx₂(t,q) * dHdx₃(t,q,μ) + ϑ₂(t,q) * d²Hdx₃dx₂(t,q,μ) - d²Hdx₂dx₂(t,q,μ) * ϑ₃(t,q) - dHdx₂(t,q,μ) * dϑ₃dx₂(t,q)
-dγ₁dx₃(t, q, μ) = dϑ₂dx₃(t,q) * dHdx₃(t,q,μ) + ϑ₂(t,q) * d²Hdx₃dx₃(t,q,μ) - d²Hdx₂dx₃(t,q,μ) * ϑ₃(t,q) - dHdx₂(t,q,μ) * dϑ₃dx₃(t,q)
-dγ₁dx₄(t, q, μ) = b₂(t,q) * dHdx₃(t,q,μ) - dHdx₂(t,q,μ) * b₃(t,q)
-dγ₂dx₁(t, q, μ) = dϑ₃dx₁(t,q) * dHdx₁(t,q,μ) + ϑ₃(t,q) * d²Hdx₁dx₁(t,q,μ) - d²Hdx₃dx₁(t,q,μ) * ϑ₁(t,q) - dHdx₃(t,q,μ) * dϑ₁dx₁(t,q)
-dγ₂dx₂(t, q, μ) = dϑ₃dx₂(t,q) * dHdx₁(t,q,μ) + ϑ₃(t,q) * d²Hdx₁dx₂(t,q,μ) - d²Hdx₃dx₂(t,q,μ) * ϑ₁(t,q) - dHdx₃(t,q,μ) * dϑ₁dx₂(t,q)
-dγ₂dx₃(t, q, μ) = dϑ₃dx₃(t,q) * dHdx₁(t,q,μ) + ϑ₃(t,q) * d²Hdx₁dx₃(t,q,μ) - d²Hdx₃dx₃(t,q,μ) * ϑ₁(t,q) - dHdx₃(t,q,μ) * dϑ₁dx₃(t,q)
-dγ₂dx₄(t, q, μ) = b₃(t,q) * dHdx₁(t,q,μ) - dHdx₃(t,q,μ) * b₁(t,q)
-dγ₃dx₁(t, q, μ) = dϑ₁dx₁(t,q) * dHdx₂(t,q,μ) + ϑ₁(t,q) * d²Hdx₂dx₁(t,q,μ) - d²Hdx₁dx₁(t,q,μ) * ϑ₂(t,q) - dHdx₁(t,q,μ) * dϑ₂dx₁(t,q)
-dγ₃dx₂(t, q, μ) = dϑ₁dx₂(t,q) * dHdx₂(t,q,μ) + ϑ₁(t,q) * d²Hdx₂dx₂(t,q,μ) - d²Hdx₁dx₂(t,q,μ) * ϑ₂(t,q) - dHdx₁(t,q,μ) * dϑ₂dx₂(t,q)
-dγ₃dx₃(t, q, μ) = dϑ₁dx₃(t,q) * dHdx₂(t,q,μ) + ϑ₁(t,q) * d²Hdx₂dx₃(t,q,μ) - d²Hdx₁dx₃(t,q,μ) * ϑ₂(t,q) - dHdx₁(t,q,μ) * dϑ₂dx₃(t,q)
-dγ₃dx₄(t, q, μ) = b₁(t,q) * dHdx₂(t,q,μ) - dHdx₁(t,q,μ) * b₂(t,q)
+dγ₁dx₁(t,q,μ) = dϑ₂dx₁(t,q) * dHdx₃(t,q,μ) + ϑ₂(t,q) * d²Hdx₃dx₁(t,q,μ) - d²Hdx₂dx₁(t,q,μ) * ϑ₃(t,q) - dHdx₂(t,q,μ) * dϑ₃dx₁(t,q)
+dγ₁dx₂(t,q,μ) = dϑ₂dx₂(t,q) * dHdx₃(t,q,μ) + ϑ₂(t,q) * d²Hdx₃dx₂(t,q,μ) - d²Hdx₂dx₂(t,q,μ) * ϑ₃(t,q) - dHdx₂(t,q,μ) * dϑ₃dx₂(t,q)
+dγ₁dx₃(t,q,μ) = dϑ₂dx₃(t,q) * dHdx₃(t,q,μ) + ϑ₂(t,q) * d²Hdx₃dx₃(t,q,μ) - d²Hdx₂dx₃(t,q,μ) * ϑ₃(t,q) - dHdx₂(t,q,μ) * dϑ₃dx₃(t,q)
+dγ₁dx₄(t,q,μ) = dϑ₂dx₄(t,q) * dHdx₃(t,q,μ) + ϑ₂(t,q) * d²Hdx₃dx₄(t,q,μ) - d²Hdx₂dx₄(t,q,μ) * ϑ₃(t,q) - dHdx₂(t,q,μ) * dϑ₃dx₄(t,q)
+dγ₂dx₁(t,q,μ) = dϑ₃dx₁(t,q) * dHdx₁(t,q,μ) + ϑ₃(t,q) * d²Hdx₁dx₁(t,q,μ) - d²Hdx₃dx₁(t,q,μ) * ϑ₁(t,q) - dHdx₃(t,q,μ) * dϑ₁dx₁(t,q)
+dγ₂dx₂(t,q,μ) = dϑ₃dx₂(t,q) * dHdx₁(t,q,μ) + ϑ₃(t,q) * d²Hdx₁dx₂(t,q,μ) - d²Hdx₃dx₂(t,q,μ) * ϑ₁(t,q) - dHdx₃(t,q,μ) * dϑ₁dx₂(t,q)
+dγ₂dx₃(t,q,μ) = dϑ₃dx₃(t,q) * dHdx₁(t,q,μ) + ϑ₃(t,q) * d²Hdx₁dx₃(t,q,μ) - d²Hdx₃dx₃(t,q,μ) * ϑ₁(t,q) - dHdx₃(t,q,μ) * dϑ₁dx₃(t,q)
+dγ₂dx₄(t,q,μ) = dϑ₃dx₄(t,q) * dHdx₁(t,q,μ) + ϑ₃(t,q) * d²Hdx₁dx₄(t,q,μ) - d²Hdx₃dx₄(t,q,μ) * ϑ₁(t,q) - dHdx₃(t,q,μ) * dϑ₁dx₄(t,q)
+dγ₃dx₁(t,q,μ) = dϑ₁dx₁(t,q) * dHdx₂(t,q,μ) + ϑ₁(t,q) * d²Hdx₂dx₁(t,q,μ) - d²Hdx₁dx₁(t,q,μ) * ϑ₂(t,q) - dHdx₁(t,q,μ) * dϑ₂dx₁(t,q)
+dγ₃dx₂(t,q,μ) = dϑ₁dx₂(t,q) * dHdx₂(t,q,μ) + ϑ₁(t,q) * d²Hdx₂dx₂(t,q,μ) - d²Hdx₁dx₂(t,q,μ) * ϑ₂(t,q) - dHdx₁(t,q,μ) * dϑ₂dx₂(t,q)
+dγ₃dx₃(t,q,μ) = dϑ₁dx₃(t,q) * dHdx₂(t,q,μ) + ϑ₁(t,q) * d²Hdx₂dx₃(t,q,μ) - d²Hdx₁dx₃(t,q,μ) * ϑ₂(t,q) - dHdx₁(t,q,μ) * dϑ₂dx₃(t,q)
+dγ₃dx₄(t,q,μ) = dϑ₁dx₄(t,q) * dHdx₂(t,q,μ) + ϑ₁(t,q) * d²Hdx₂dx₄(t,q,μ) - d²Hdx₁dx₄(t,q,μ) * ϑ₂(t,q) - dHdx₁(t,q,μ) * dϑ₂dx₄(t,q)
 
 
 function v₁(t, q, v, params)
-    v[1] = + dβ₃dx₂(t,q)
-    v[2] = - dβ₃dx₁(t,q)
+    @unpack μ = params
+    v[1] = + dβ₃dx₂(t,q,μ)
+    v[2] = - dβ₃dx₁(t,q,μ)
     v[3] = 0
     v[4] = 0
 end
 
 function v₂(t, q, v, params)
-    v[1] = - dβ₂dx₃(t,q)
+    @unpack μ = params
+    v[1] = - dβ₂dx₃(t,q,μ)
     v[2] = 0
-    v[3] = + dβ₂dx₁(t,q)
+    v[3] = + dβ₂dx₁(t,q,μ)
     v[4] = 0
 end
 
 function v₃(t, q, v, params)
+    @unpack μ = params
     v[1] = 0
-    v[2] = + dβ₁dx₃(t,q)
-    v[3] = - dβ₁dx₂(t,q)
+    v[2] = + dβ₁dx₃(t,q,μ)
+    v[3] = - dβ₁dx₂(t,q,μ)
     v[4] = 0
 end
 
@@ -310,9 +322,9 @@ end
 
 function v(t, q, v, params)
     @unpack μ = params
-    v[1] =   dγ₁dx₄(t,q,μ) + dβ₃dx₂(t,q)   - dβ₂dx₃(t,q)
-    v[2] =   dγ₂dx₄(t,q,μ) + dβ₁dx₃(t,q)   - dβ₃dx₁(t,q)
-    v[3] =   dγ₃dx₄(t,q,μ) + dβ₂dx₁(t,q)   - dβ₁dx₂(t,q)
+    v[1] =   dγ₁dx₄(t,q,μ) + dβ₃dx₂(t,q,μ) - dβ₂dx₃(t,q,μ)
+    v[2] =   dγ₂dx₄(t,q,μ) + dβ₁dx₃(t,q,μ) - dβ₃dx₁(t,q,μ)
+    v[3] =   dγ₃dx₄(t,q,μ) + dβ₂dx₁(t,q,μ) - dβ₁dx₂(t,q,μ)
     v[4] = - dγ₁dx₁(t,q,μ) - dγ₂dx₂(t,q,μ) - dγ₃dx₃(t,q,μ)
     nothing
 end
